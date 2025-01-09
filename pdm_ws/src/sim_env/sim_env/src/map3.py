fc0f6611f39b3d81ef5803e458efa4fb5fbf3056
Adithya Abhilash <adithya.abhi21@gmail.com>
2025-01-09 18:19:18 +0100

    Maps 1-4 w/o robot pos

--- pdm_ws/src/sim_env/sim_env/src/map3.py
+++ pdm_ws/src/sim_env/sim_env/src/map3.py
@@ -1,6 +1,9 @@
 import warnings
 import gymnasium as gym
+import matplotlib.pyplot as plt
 import numpy as np
+import pybullet as p
+import cv2 as cv
 
 from urdfenvs.robots.generic_urdf import GenericUrdfReacher
 from urdfenvs.urdf_common.urdf_env import UrdfEnv
@@ -28,12 +31,45 @@ class Panda_Sym:
         self.init_pos = np.array([4.0,-14.0,0.0])
         self.env: UrdfEnv = None
         self.Gen_Env()
+        img = self.capture_overhead_image(self.env)
 
-        self.run_point_robot_with_occupancy_sensor()
+        cv.imwrite("home/adi/Desktop/maps/Map.png", img)
+
+        cv.imshow('image window', img)
+        # add wait key. window waits until user presses a key
+        cv.waitKey(0)
+        #   and finally destroy/close all open windows
+        cv.destroyAllWindows()
+
+        history = self.run_point_robot_with_occupancy_sensor()
     
     def __del__(self):
         if self.env is not None:
             self.env.close()
+
+    def capture_overhead_image(self, environment,width = 640, height = 480, fov = 60, near_val = 0.1, far_val = 100.0,
+    camera_position = (10.0, -8.0, 14), target_position = (10.0, -8.0, 0), up_vector = (0, 2, 0)):
+        view_matrix  = p.computeViewMatrix(cameraEyePosition=camera_position, cameraTargetPosition=target_position, cameraUpVector=up_vector)
+
+        projection_matrix =p.computeProjectionMatrixFOV(
+        fov=fov,
+        aspect=float(width) / height,
+        nearVal=near_val,
+        farVal=far_val)
+
+        _, _, rgb_image, _, _ = p.getCameraImage(
+        width=width,
+        height=height,
+        viewMatrix=view_matrix,
+        projectionMatrix=projection_matrix,
+        renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
+        gray = cv.cvtColor(rgb_image, cv.COLOR_BGR2GRAY)
+        ret,thresh = cv.threshold(gray,250,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+        #print(thresh)
+        return np.reshape(thresh, (height, width))[:, :]
+
+# OCCUPANCY MAP SECTION
 
     def get_index_from_coordinates(self,point, mesh) -> tuple:
         distances = np.linalg.norm(mesh - point, axis=3)
@@ -43,7 +79,7 @@ class Panda_Sym:
         index = list(self.get_index_from_coordinates(point, mesh))
         return occupancy[tuple(index)]
 
-    def run_point_robot_with_occupancy_sensor(self, n_steps=10):
+    def run_point_robot_with_occupancy_sensor(self,n_steps=10, render=False, obstacles=True, goal=True):
         
         # add sensor
         val = 80
@@ -58,6 +94,7 @@ class Panda_Sym:
         # Set spaces AFTER all components have been added.
         self.env.set_spaces()
         defaultAction = np.array([1.0,-1.0,0.0])
+        pos0 = np.array([13.0,-14.0,0.0])
         vel0 = np.array([0.0, 0.0, 0.0])
         initial_observations = []
         self.ob, _ = self.env.reset(mount_positions=np.array([self.init_pos]), vel=vel0)
@@ -70,12 +107,34 @@ class Panda_Sym:
         )
         point=[]
         action = np.zeros(self.env.n())
+        history = []
+        final_occupancy = None
         for _ in range(n_steps):
             action[:3] = defaultAction
             self.ob, *_ = self.env.step(action)
             point = np.append(self.ob['robot_0']['joint_state']['position'][0:2], 0.0)
             occupancy = self.ob['robot_0']['Occupancy']
             occupancy_eval = self.evaluate_occupancy(point, sensor.mesh(), occupancy, [0.2, 0.2, 1])
+            print(occupancy_eval)
+            final_occupancy = occupancy
+            history.append(self.ob)
+
+        #if(final_occupancy!= None):
+        np.save("occupancy_grid.npy", final_occupancy)
+        print("The occupancy map has been saved")
+        slice_index = 1
+        slice_data = final_occupancy[:,:,slice_index]
+
+        plt.imshow(slice_data, cmap="gray", origin="lower")
+        plt.title(f"Occupancy map")
+        plt.show()
+
+
+        self.env.close()
+        return history
+
+# OCCUPANCY MAP SECTION ENDS
+
 
     def Gen_Env(self):
         self.Gen_Panda()
@@ -95,10 +154,10 @@ class Panda_Sym:
         self.env.add_obstacle(self.obstacle_room2)
         #self.env.add_obstacle(self.sphereObst1)
         self.env.add_obstacle(self.shelf1)
-        #self.env.add_obstacle(self.shelf2)
+        self.env.add_obstacle(self.shelf2)
         self.env.add_obstacle(self.shelf3)
         self.env.add_obstacle(self.shelf4)
-        #self.env.add_obstacle(self.shelf5)
+        self.env.add_obstacle(self.shelf5)
         self.env.add_obstacle(self.shelf6)
         #action = np.zeros(self.env.n())
         self.ob = self.env.reset(mount_positions=np.array([self.init_pos]))
@@ -190,7 +249,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.0, 1.0, 0.0, 1.0],
                 'geometry': {
-                    'position': [5.0, -5.0 , 0.4], 
+                    'position': [10.0, -8.0 , 0.4], 
                     'width': self.obstacle_size, 'height': self.obstacle_size, 'length': self.obstacle_size
                 }
             }))
@@ -200,7 +259,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.0, 1.0, 0.0, 1.0],
                 'geometry': {
-                    'position': [17.0, -8.0, 0.4],
+                    'position': [10.0, -6.0, 0.4],
                     'width': self.obstacle_size, 'height': self.obstacle_size, 'length': self.obstacle_size
                 }
             }))
@@ -218,7 +277,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.5, 0.5, 0.0, 1.0],
                 'geometry': {
-                    'position': [self.boundary_size/2 -2.0, -4.0, 0.4], 
+                    'position': [self.boundary_size/2 -2.0, -7.5, 0.4], 
                     'width':  4, 'height': self.height, 'length': self.obstacle_size+0.5
                 }
             }))
@@ -228,7 +287,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.5, 0.5, 0.0, 1.0],
                 'geometry': {
-                    'position': [self.boundary_size/2, -4.0, 0.4], 
+                    'position': [self.boundary_size/2+2.0, -2.0, 0.4], 
                     'width':  4, 'height': self.height, 'length': self.obstacle_size+0.5
                 }
             }))
@@ -238,7 +297,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.5, 0.5, 0.0, 1.0],
                 'geometry': {
-                    'position': [self.boundary_size/2 +2.0, -4.0, 0.4], 
+                    'position': [self.boundary_size/2 +2.0, -7.5, 0.4], 
                     'width':  4, 'height': self.height, 'length': self.obstacle_size+0.5
                 }
             }))
@@ -248,7 +307,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.5, 0.5, 0.0, 1.0],
                 'geometry': {
-                    'position': [self.boundary_size/2 -2.0, -16/2-4.0, 0.4], 
+                    'position': [self.boundary_size/2 -2.0, -13.0, 0.4], 
                     'width':  4, 'height': self.height, 'length': self.obstacle_size+0.5
                 }
             }))
@@ -258,7 +317,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.5, 0.5, 0.0, 1.0],
                 'geometry': {
-                    'position': [self.boundary_size/2, -16/2-4.0, 0.4], 
+                    'position': [self.boundary_size/2-2.0, -2.0, 0.4], 
                     'width':  4, 'height': self.height, 'length': self.obstacle_size+0.5
                 }
             }))
@@ -268,7 +327,7 @@ class Panda_Sym:
                 'type': 'box', 
                 "rgba": [0.5, 0.5, 0.0, 1.0],
                 'geometry': {
-                    'position': [self.boundary_size/2 +2.0, -16/2-4.0, 0.4], 
+                    'position': [self.boundary_size/2 +2.0, -13.0, 0.4], 
                     'width':  4, 'height': self.height, 'length': self.obstacle_size+0.5
                 }
             }))
