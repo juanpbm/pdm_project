a4542ef2df603e6fabd82b139b31d41441e4139b
juanpbm <31747012+juanpbm@users.noreply.github.com>
2025-01-12 12:20:35 +0100

    Update control.py

--- pdm_ws/src/control/control/src/control.py
+++ pdm_ws/src/control/control/src/control.py
@@ -322,9 +322,6 @@ class Controller:
         
         orientation_error=self.calc_rot_error(target_orientation_matrix, current_orientation_matrix)
         desired_velocity_orientation = self.Kp * orientation_error
-        
-        # if np.linalg.norm(desired_velocity_xyz) > self.arm_max_vel:
-        #     desired_velocity_xyz = desired_velocity_xyz / np.linalg.norm(desired_velocity_xyz) * self.arm_max_vel
 
         desired_velocity = np.hstack((desired_velocity_xyz, desired_velocity_orientation)) 
         
